125a1044609daeb6d796a9343cdccf844f6c1d7d
yimej <elizabethjyim@gmail.com>
2022-01-12 15:34:56 -0500

    back

--- ap/js/scripts.js
+++ ap/js/scripts.js
@@ -41,17 +41,6 @@ splide.mount();
 
 //////////////////////// functions ////////////////////////
 
-// Get the viewport height and multiply it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then set the custom --vh value to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // Update the element's size
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 function toggleMenu(x) {
   x.classList.toggle('change');
   $('#dropup-content').toggle('slide', {direction: 'down'});
